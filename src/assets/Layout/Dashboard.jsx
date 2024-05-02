108fab99114e89f3bdd0d5990344f958b8d7ae20
palash sarkar <spalash521@gmail.com>
2024-05-03 00:22:15 +0300

    add al admin secure

--- src/assets/Layout/Dashboard.jsx
+++ src/assets/Layout/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaPaypal, FaSearch, FaShoppingCart, FaUsers, FaUtensilSpoon } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../../hooks/useCart";
+import useAdmin from "../../hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
     // TODO get isAdmin value from database
-    const isAdmin = true;
+    const [isAdmin] = useAdmin();
 
     return (
         <div className="flex">
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,6 @@
 import axios from "axios"
+import {useNavigate} from 'react-router-dom'
+import useAuth from "./useAuth";
 
 
 const axiosSecure = axios.create({
@@ -6,9 +8,12 @@ const axiosSecure = axios.create({
 })
     
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const {logOut} = useAuth()
+
     axiosSecure.interceptors.request.use(function (config) {
         const token  = localStorage.getItem('access-token')
-        console.log('request stopped by interceptores', token)
+        // console.log('request stopped by interceptores', token)
         config.headers.authorization = `Bearer ${token}`;
     // Do something before request is sent
     return config;
@@ -20,9 +25,14 @@ const useAxiosSecure = () => {
 // interceptors 401 and  403 status
     axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, (error) => {
+    }, async(error) => {
         const status = error.response.status;
-        console.log("Status error intercepted", status)
+        // console.log("Status error intercepted", status)
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login')
+         }
+
         return Promise.reject(error);
     } )
 
